e7891ca9d271aa4ed354ee6b70281bbdeeb73653
NicholasLauer <lauern@matc.edu>
2026-09-10 21:41:11 -0500

    Completed Activity 2

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a869dace87ced0d41016765d7354ccb080734d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60d3347a0f670b2e4dad1984e279b256befa3a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
