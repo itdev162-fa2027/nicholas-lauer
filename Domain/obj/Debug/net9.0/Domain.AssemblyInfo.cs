dd270a6bda8b86dc4c42e78e8920f5bf90a37434
NicholasLauer <lauern@matc.edu>
2026-09-14 23:23:11 -0500

    Replaced minimal API with Controller API

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60d3347a0f670b2e4dad1984e279b256befa3a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7891ca9d271aa4ed354ee6b70281bbdeeb73653")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
